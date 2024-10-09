3640475c059e847a04cddf8d077abd4b13ceeead
Rogier van Staveren <rogier@batoid.com>
2024-10-09 13:02:04 +0200

    Add configurable receive interrupt pin

--- components/cc2500/__init__.py
+++ components/cc2500/__init__.py
@@ -1,7 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
-from esphome.const import CONF_ID
+from esphome import pins
+from esphome.const import CONF_ID, CONF_NUMBER
+from esphome.core import CORE
 
 DEPENDENCIES = ["spi"]
 CODEOWNERS = ["@rrooggiieerr"]
@@ -11,12 +13,22 @@ CC2500Component = cc2500_ns.class_("CC2500Component", cg.Component, spi.SPIDevic
 CC2500Device = cc2500_ns.class_("CC2500Device")
 
 CONF_CC2500_ID = 'cc2500_id'
+CONF_RECEIVE_INTERRUPT_PIN = 'receive_interrupt_pin'
 CONF_OUTPUT_POWER = 'output_power'
 # CONF_SNIFF_AFTER_X_COMMANDS = 'sniff_after_x_commands'
+
+def validate_receive_interrupt_pin(value):
+    value = pins.internal_gpio_input_pin_schema(value)
+    if CORE.is_esp8266 and value[CONF_NUMBER] >= 16:
+        raise cv.Invalid(
+            "Pins GPIO16 and GPIO17 cannot be used as receive interrupt pin on ESP8266."
+        )
+    return value
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(CC2500Component),
+        cv.Optional(CONF_RECEIVE_INTERRUPT_PIN): validate_receive_interrupt_pin,
         cv.Optional(CONF_OUTPUT_POWER, default=255): cv.uint8_t,
         # cv.Optional(CONF_SNIFF_AFTER_X_COMMANDS, default=5): cv.uint16_t,
     }
@@ -24,6 +36,9 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    if CONF_RECEIVE_INTERRUPT_PIN in config:
+        pin = await cg.gpio_pin_expression(config[CONF_RECEIVE_INTERRUPT_PIN])
+        cg.add(var.set_receive_interrupt_pin(pin))
     if CONF_OUTPUT_POWER in config:
         cg.add(var.set_output_power(config[CONF_OUTPUT_POWER]))
     # if CONF_SNIFF_AFTER_X_COMMANDS in config:
